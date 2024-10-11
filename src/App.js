b9902e37fe55dca0dcb4d8aea3e63a9707358074
Anadi Mishra <kuromishra@gmail.com>
2024-10-10 19:37:54 -0700

    Base Map implemented with hardcoded markers

--- src/App.js
+++ src/App.js
@@ -1,3 +1,68 @@
+import "./styles.css";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
+
+import { Icon, divIcon, point } from "leaflet";
+
+// create custom icon marker
+const customIcon = new Icon({
+  iconUrl: require("./icons/marker.png"),
+  iconSize: [24, 36] // size of the icon
+});
+
+// custom cluster icon
+const createClusterCustomIcon = function (cluster) {
+  return new divIcon({
+    html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
+    className: "custom-marker-cluster",
+    iconSize: point(33, 33, true)
+  });
+};
+
+// hard-coded markers
+const markers = [
+  {
+    geocode: [49.2837, -123.1211],
+    popUp: "Fairmont, Vancouver"
+  },
+  {
+    geocode: [49.284530, -123.101880],
+    popUp: "Waterfront, Vancouver"
+  },
+  {
+    geocode: [49.278430, -123.112680],
+    popUp: "Stadium-Chinatown"
+  }
+];
+
+export default function App() {
+  return (
+    <MapContainer center={[49.2827, -123.1207]} zoom={15}>
+      {/* OPEN STREEN MAPS TILES */}
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <MarkerClusterGroup
+        chunkedLoading
+        iconCreateFunction={createClusterCustomIcon}
+      >
+        {/* Mapping through the markers */}
+        {markers.map((marker) => (
+          <Marker position={marker.geocode} icon={customIcon}>
+            <Popup>{marker.popUp}</Popup>
+          </Marker>
+        ))}
+
+      </MarkerClusterGroup>
+    </MapContainer>
+  );
+}
+
+
+/* Eventual Set up
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
@@ -23,3 +88,4 @@ function App() {
 }
 
 export default App;
+*/
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1>Atlas Spark - Team 3</h1>
     <App />
   </React.StrictMode>
 );
